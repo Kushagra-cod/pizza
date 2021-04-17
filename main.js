088cc943bfec2cdd943c520613a8aa8e609317d8
Kushagra-cod <81215175+Kushagra-cod@users.noreply.github.com>
2021-04-17 16:30:06 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,27 @@
+name_of_the_pizza_array = [];
+
+function submit()
+{
+    var name_1 = document.getElementById("name_of_pizza_1").value;
+    var name_2 = document.getElementById("name_of_the_pizza_2").value;
+    var name_3 = document.getElementById("name_of_the_pizza_3").value;
+    var name_4 = document.getElementById("name_of_the_pizza_4").value;
+
+    name_of_the_array_array.push(name_1);
+    name_of_the_pizza_array.push(name_2);
+    name_of_the_pizza_array.push(name_3);
+    name_of_the_pizza_array.push(name_4);
+
+    console.log (name_of_the_student_array);
+
+    document.getElementById("display_name").innerHTML = name_of_the_pizza_array;
+    document.getElementById("submit_button").style.display = "none";
+}
+function sorting()
+{
+    name_of_the_pizza_array.sort();
+
+console.log(name_of_the_pizza_array);
+
+document.getElementById("display_name").innerHTML = name_of_the_pizza_array;
+}
